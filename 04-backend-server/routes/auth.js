fd1e719542b26bf6bf02697216fd6a49148d4dd2
BlackRoadCode <blackroadcode@gmail.com>
2021-01-12 17:33:51 -0600

    Implementado google auth

--- 04-backend-server/routes/auth.js
+++ 04-backend-server/routes/auth.js
@@ -3,7 +3,7 @@
 
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { login } = require('../controllers/auth');
+const { login, googleSignIn } = require('../controllers/auth');
 const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
@@ -13,6 +13,11 @@ router.post( '/', [
     check('email', 'El campo email es obligatorio').isEmail(),
     validarCampos,
  ] , login );
+
+ router.post( '/google', [ 
+    check('token', 'El token de google es obligatorio').not().isEmpty(),
+    validarCampos,
+ ] , googleSignIn );
 
 
 module.exports = router;
